7e10bb538555f4781c9d02b234c182dbbb9a6d7c
Markus <markus.tyrkko@gmail.com>
2025-09-07 14:48:59 +0300

    Improve styling

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -2,8 +2,15 @@ import styles from "./Footer.module.scss";
 
 const Footer = () => (
   <div className={styles.footer}>
-    <span>&copy; {new Date().getFullYear()} Kasvun mahdollistajat Oy</span>
-    <span>2745834-5</span>
+    <span>&copy; {new Date().getFullYear()}</span>
+    <div>
+      <p>Kasvun mahdollistajat Oy</p>
+      <p>2745834-5</p>
+    </div>
+    <div>
+      <p>Katri Sipilä Consulting Oy</p>
+      <p>3105526-6</p>
+    </div>
   </div>
 );
 
