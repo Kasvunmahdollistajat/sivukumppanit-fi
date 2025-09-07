fb23d6f4d5ee4474b4d3efd83ab3d4e35f404139
Markus <markus.tyrkko@gmail.com>
2025-09-07 14:51:49 +0300

    Updates

---
+++ _next/static/chunks/pages/components/Footer-1425fc886c0eefdd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[290],{500:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Footer",function(){return e(5566)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(500)),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/components/NavBar-0451d2d7f8cd8c54.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[293],{9192:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/NavBar",function(){return e(8361)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(9192)),_N_E=_.O()}]);
